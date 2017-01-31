9067858fd0599f6420903e372f104d940876e3af
limerixtt123 <limerix@mail.bg>
2017-01-31 11:03:55 +0200

    Java Syntax Lab

---
+++ 01JavaSyntaxLab/_08Lottery.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class _08Lottery {
+    public static void main(String[] args) {
+        for (int i1 = 1; i1 <= 8; i1++)
+            for (int i2 = i1 + 1; i2 <= 9; i2++)
+                for (int i3 = i2 + 1; i3 <= 10; i3++)
+                    System.out.printf("%d %d %d\n", i1, i2, i3);
+    }
+}
