8667bbd70ea03cbdc036515c68c32edc34120286
limerixtt123 <limerix@mail.bg>
2017-01-31 11:02:37 +0200

    Java Syntax Lab

---
+++ 01JavaSyntaxLab/_04Greeting.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class _04Greeting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String firstName = scanner.nextLine();
+        String lastName = scanner.nextLine();
+        if (firstName.isEmpty()) {
+            firstName = "*****";
+        }
+        if (lastName.isEmpty()) {
+            lastName = "*****";
+        }
+        System.out.printf("Hello, %s %s!", firstName, lastName);
+    }
+}
