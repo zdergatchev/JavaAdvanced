5b03bb296269973dc02c168d4b5c95a14f115c71
limerixtt123 <limerix@mail.bg>
2017-01-31 11:03:15 +0200

    Java Syntax Lab

---
+++ 01JavaSyntaxLab/_06Numbers0_9.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class _06Numbers0_9 {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter <= 9) {
+            System.out.printf("Number: %d%n", counter);
+            counter++;
+        }
+    }
+}
