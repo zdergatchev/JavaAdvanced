1ea747bf729b92716a6650efe4cc119bb54ea7b0
limerixtt123 <limerix@mail.bg>
2017-02-07 10:18:45 +0200

    Matrices Exercises

---
+++ 04MatricesExercises/_07CollectTheCoins.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class _07CollectTheCoins {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        char[][] matrix = new char[4][];
+        for (int i = 0; i < 4; i++) {
+            char[] currentRow = scanner.nextLine().toCharArray();
+            matrix[i] = new char[currentRow.length];
+            for (int j = 0; j < currentRow.length; j++) {
+                matrix[i][j] = currentRow[j];
+            }
+        }
+
+        char[] moves = scanner.nextLine().toCharArray();
+        int currentRow = 0;
+        int currentCol = 0;
+        int coins = 0;
+        int wallsHit = 0;
+
+        if (matrix[currentRow][currentCol] == '$') {
+            coins++;
+        }
+        for (int i = 0; i < moves.length; i++) {
+            int copyRow = currentRow;
+            int copyCol = currentCol;
+
+            switch (moves[i]) {
+                case '^':
+                    currentRow--;
+                    break;
+                case '>':
+                    currentCol++;
+                    break;
+                case 'V':
+                    currentRow++;
+                    break;
+                case '<':
+                    currentCol--;
+                    break;
+            }
+
+            if (!isInMatrix(currentRow, currentCol, matrix)) {
+                currentRow = copyRow;
+                currentCol = copyCol;
+                wallsHit++;
+            } else {
+                if (matrix[currentRow][currentCol] == '$')
+                    coins++;
+            }
+        }
+        System.out.println("Coins = " + coins);
+        System.out.println("Walls = " + wallsHit);
+    }
+
+    private static boolean isInMatrix(int currentRow, int currentCol, char[][] matrix) {
+        if (currentRow >= 0 && currentCol >= 0 && currentRow < 4 && currentCol < matrix[currentRow].length) {
+            return true;
+        }
+        return false;
+    }
+}
