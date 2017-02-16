254a841ad39f4d77d70037f9331a0623415cfbb9
limerixtt123 <limerix@mail.bg>
2017-02-16 14:07:04 +0200

    Manual String Lab

---
+++ 11ManualStringLab/_05ConcatenatingStrings.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class _05ConcatenatingStrings {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+       // ArrayList<ArrayList<Long>> result =
+
+
+    }
+}
