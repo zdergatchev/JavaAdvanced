5b999abfa35ddff7fe4435e9fdb798096d729a08
limerixtt123 <limerix@mail.bg>
2017-02-17 09:17:21 +0200

    Functional Programming Exercises

---
+++ 16FunctionalProgrammingExercises/_03CustomMinFunction.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class _03CustomMinFunction {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = reader.readLine().split("\\s+");
+        int[] numbers = new int[input.length];
+
+        for (int i = 0; i < input.length; i++) {
+            numbers[i] = Integer.parseInt(input[i]);
+        }
+
+        Arrays.sort(numbers);
+        Function<int[], Integer> minNumber = x -> x[0];
+        Consumer<Integer> print = x -> System.out.println(x + " ");
+        print.accept(minNumber.apply(numbers));
+    }
+}
